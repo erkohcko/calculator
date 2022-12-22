ac78cdee09cc93a65e91dcc183d971f9282b3ee8
erkohcko <112120806+erkohcko@users.noreply.github.com>
2022-12-22 20:54:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,11 @@ public class Main {
 
         public static void main(String[] args) throws IOException {
             scan = new Scanner(System.in);
-            System.out.print("Result: \n" +  calc(" "));
-
+            System.out.print("Result: \n" +  calc(scan.nextLine()));
         }
-
         public static String calc(String input) throws IOException {
-            String result = " ";
-            String calculation = scan.nextLine();
-            String[] nums = calculation.split(" ");
+            String result;
+            String[] nums = input.split(" ");
             if (nums.length < 3) {
                 throw new IOException("Неверный формат, требуется пробел между операндами и оператором");
             }
@@ -21,33 +18,26 @@ public class Main {
             }
             int num1 = Integer.parseInt(nums[0]);
             int num2 = Integer.parseInt(nums[2]);
-            if (num1 < 1 || num1 > 10) {
+            if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
                 throw new IOException("Неверный формат математической операции, укажите числа от 1 до 10 включительно");
             }
-            if (num2 < 1 || num2 > 10) {
-                throw new IOException("Неверный формат математической операции, укажите числа от 1 до 10 включительно");
+                String operation = nums[1];
+                switch (operation) {
+                    case "+":
+                        result = Integer.toString(num1 + num2);
+                        break;
+                    case "-":
+                        result = Integer.toString(num1 - num2);
+                        break;
+                    case "*":
+                        result = Integer.toString(num1 * num2);
+                        break;
+                    case "/":
+                        result = Integer.toString(num1 / num2);
+                        break;
+                    default:
+                        throw new IOException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                }
+                return result;
             }
-            String operation = nums[1];
-            switch (operation) {
-                case "+":
-                    result = Integer.toString(num1 + num2);
-                    break;
-                case "-":
-                    result = Integer.toString(num1 - num2);
-                    break;
-                case "*":
-                    result = Integer.toString(num1 * num2);
-                    break;
-                case "/":
-                    result = Integer.toString(num1 / num2);
-                    break;
-                default:
-                    throw new IOException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-
-            }
-            return result;
-
         }
-    }
-
-
